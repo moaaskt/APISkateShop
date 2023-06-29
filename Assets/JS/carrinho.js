c26e6996522571768f5bfbde872ccf77a05c261a
moaaskt <moacirmayer@hotmail.com>
2023-06-29 02:21:30 -0300

    atulizando projeto

---
+++ Assets/JS/carrinho.js
@@ -0,0 +1,46 @@
+function mostrarCarrinho() {
+    // Acessar o elemento da tabela no HTML
+    const tabelaCarrinho = document.getElementById('tabela-carrinho');
+  
+    // Limpar a tabela
+    tabelaCarrinho.innerHTML = '';
+  
+    // Iterar sobre os itens do carrinho
+    carrinho.forEach((skate, index) => {
+      const linha = tabelaCarrinho.insertRow();
+  
+      // Adicionar as células com as informações do skate
+      const colunaShape = linha.insertCell();
+      colunaShape.textContent = skate.shape;
+  
+      const colunaRodas = linha.insertCell();
+      colunaRodas.textContent = skate.rodinha;
+  
+      const colunaRolamentos = linha.insertCell();
+      colunaRolamentos.textContent = skate.rolamentos;
+  
+      const colunaTrucks = linha.insertCell();
+      colunaTrucks.textContent = skate.trucks;
+  
+      const colunaLixa = linha.insertCell();
+      colunaLixa.textContent = skate.lixa;
+  
+      const colunaParafusos = linha.insertCell();
+      colunaParafusos.textContent = skate.parafusos;
+  
+      const colunaPreco = linha.insertCell();
+      colunaPreco.textContent = skate.preco;
+      
+      const colunaRemover = linha.insertCell();
+      const botaoRemover = document.createElement('button');
+      botaoRemover.textContent = 'Remover';
+      botaoRemover.addEventListener('click', () => removerDoCarrinho(index));
+      colunaRemover.appendChild(botaoRemover);
+    });
+  }
+  
+  function removerDoCarrinho(index) {
+    carrinho.splice(index, 1);
+    mostrarCarrinho();
+  }
+  
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,3 @@
-// Função para fazer uma requisição GET à API e exibir os skates
 function getSkates() {
   fetch('http://localhost:4000/inicio')
     .then(response => response.json())
@@ -8,6 +7,7 @@ function getSkates() {
 
       skates.forEach(skate => {
         const skateItem = document.createElement('div');
+        skateItem.classList.add('card');
         skateItem.innerHTML = `
           <h3>${skate.shape}</h3>
           <p>Rodas: ${skate.rodinha}</p>
@@ -17,14 +17,81 @@ function getSkates() {
           <p>Parafusos: ${skate.parafusos}</p>
           <p>Preço: ${skate.preco}</p>
           <img class="produto-imagem" src="${skate.imagem}" alt="Imagem do skate">
+          <div class="card-buttons">
+            <button class="btn btn-primary buy-button">Comprar</button>
+            <button class="btn btn-secondary espionar-button" data-skate='${JSON.stringify(skate)}'>Espionar</button>
+          </div>
         `;
         skateList.appendChild(skateItem);
+      });
+
+      const buyButtons = document.getElementsByClassName('buy-button');
+      Array.from(buyButtons).forEach(button => {
+        button.addEventListener('click', () => {
+          const skateData = JSON.parse(button.parentNode.getAttribute('data-skate'));
+          buySkate(skateData);
+        });
+      });
+
+      const espionarButtons = document.getElementsByClassName('espionar-button');
+      Array.from(espionarButtons).forEach(button => {
+        button.addEventListener('click', () => {
+          const skateData = JSON.parse(button.getAttribute('data-skate'));
+          showPopup(skateData);
+        });
       });
     })
     .catch(error => {
       console.error('Erro ao obter skates:', error);
     });
 }
+
+function showPopup(skateData) {
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+  popup.innerHTML = `
+    <div class="popup-content">
+      <h3>${skateData.shape}</h3>
+      <p>Rodas: ${skateData.rodinha}</p>
+      <p>Rolamentos: ${skateData.rolamentos}</p>
+      <p>Trucks: ${skateData.trucks}</p>
+      <p>Lixa: ${skateData.lixa}</p>
+      <p>Parafusos: ${skateData.parafusos}</p>
+      <p>Preço: ${skateData.preco}</p>
+      <img class="popup-imagem" src="${skateData.imagem}" alt="Imagem do skate">
+      <button class="btn btn-primary close-button">x</button>
+    </div>
+  `;
+
+  const closePopup = () => {
+    document.body.removeChild(popup);
+  };
+
+  const closeButton = popup.querySelector('.close-button');
+  closeButton.addEventListener('click', closePopup);
+
+  document.body.appendChild(popup);
+}
+
+let carrinho = [];
+
+
+
+function buySkate(skateData) {
+  // Verificar se o skate já está no carrinho
+  const skateExistente = carrinho.find(item => item.shape === skateData.shape);
+  
+  if (skateExistente) {
+    console.log('Skate já está no carrinho:', skateData);
+    return;
+  }
+  
+  // Adicionar o skate ao carrinho
+  carrinho.push(skateData);
+  console.log('Skate adicionado ao carrinho:', skateData);
+}
+
+
 $(document).ready(function() {
   $('.carousel').carousel({
     interval: 3000 // Tempo em milissegundos entre cada transição de slide (5 segundos neste exemplo)
@@ -32,3 +99,7 @@ $(document).ready(function() {
 });
 // Chamada inicial para obter os skates quando a página é carregada
 getSkates();
+
+
+
+
